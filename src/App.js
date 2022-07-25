d2488a17be0834b2636f7ba0aeb3e992085bfe79
Ahmed-Ansari02 <ansari82002@gmail.com>
2022-07-25 18:15:00 -0500

    moved state to board component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import "./App.css";
 import Board from "./components/Board";
-import { useState, useEffect } from "react";
 
 function App() {
-  let player;
-  const [marked, setmarked] = useState([]);
-  function setMarked(id) {
-    player = player === "X" ? "O" : "X";
-    setmarked((marked) => [...marked, { id, player: player }]);
-  }
-  useEffect(() => console.log(marked, player), [marked]);
   return (
     <div className="App">
-      <Board setMarked={setMarked} marked={marked}/>
+      <Board />
     </div>
   );
 }
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,15 +1,21 @@
 import React from "react";
 import Square from "./Square";
 import "./component_style.css";
-import { useState } from "react";
+
+import { useState, useEffect } from "react";
 let Squares = [];
-function Board({setMarked, marked}) {
-  function onclick(id) {
-    setMarked(id);
+function Board() {
+  let player;
+  const [marked, setmarked] = useState([]);
+  function setMarked(id) {
+    player = player === "X" ? "O" : "X";
+    setmarked((marked) => [...marked, { id, player: player }]);
   }
-  if (Squares.length===0) {
+  useEffect(() => console.log(marked, player), [marked]);
+
+  if (Squares.length === 0) {
     for (let i = 0; i < 9; i++) {
-      Squares = [...Squares, <Square id={i} key={i} onclick={onclick} />];
+      Squares = [...Squares, <Square id={i} key={i} onclick={setMarked} />];
     }
   }
   return <div className="board">{Squares}</div>;
